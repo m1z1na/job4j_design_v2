142edf63b11625522c7c68d50c73643f1f2524ba
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-11-13 10:57:02 +0300

    0. Принцип единственной ответственности [#4913]

--- src/main/java/ru/job4j/ood/spr/UserManager.java
+++ src/main/java/ru/job4j/ood/spr/UserManager.java
@@ -11,29 +11,29 @@ public class UserManager {
         System.out.println("вы удалили пользователя");
     }
     void sendMailToUser(){
-       /* String to = "receive@abc.om";         // sender email
-        String from = "sender@abc.com";       // receiver email
-        String host = "127.0.0.1";            // mail server host
+       /* String to = "receive@abc.om";           sender email
+        String from = "sender@abc.com";         receiver email
+        String host = "127.0.0.1";              mail server host
 
         Properties properties = System.getProperties();
         properties.setProperty("mail.smtp.host", host);
 
-        Session session = Session.getDefaultInstance(properties); // default session
+        Session session = Session.getDefaultInstance(properties);   default session
 
         try {
-            MimeMessage message = new MimeMessage(session); // email message
+            MimeMessage message = new MimeMessage(session);   email message
 
-            message.setFrom(new InternetAddress(from)); // setting header fields
+            message.setFrom(new InternetAddress(from));  setting header fields
 
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            message.setSubject("Test Mail from Java Program"); // subject line
+            message.setSubject("Test Mail from Java Program");  subject line
 
-            // actual mail body
+              actual mail body
             message.setText("You can send mail from Java program by using mail API, but you need" +
                     "couple of more JAR files e.g. smtp.jar and activation.jar");
 
-            // Send message
+            Send message
             Transport.send(message); System.out.println("Email Sent successfully....");
         } catch (MessagingException mex){ mex.printStackTrace(); }
 
